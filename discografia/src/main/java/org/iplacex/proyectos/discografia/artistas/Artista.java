81b1c4ceb9be0a038a4625aabeec2e45f18bba5a
Yani-cnvs <yani.contreras.v@gmail.com>
2025-09-15 16:31:15 -0300

    modificaciones para evitar errores

--- discografia/src/main/java/org/iplacex/proyectos/discografia/artistas/Artista.java
+++ discografia/src/main/java/org/iplacex/proyectos/discografia/artistas/Artista.java
@@ -1,17 +1,32 @@
 package org.iplacex.proyectos.discografia.artistas;
 
-import java.util.List;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document("artistas")
+@Document(collection = "artistas")
 public class Artista {
-    
+
     @Id
-    public String _id;
-    public String nombre;
-    public List<String> estilos;
-    public int anioFundacion;
-    public boolean estaActivo;
+    private String _id;
+    private String nombre;
+    private String estilos;
+    private int anioFundacion;
+    private boolean estaActivo;
+
+    public Artista() {}
+
+    public String get_id() { return _id; }
+    public void set_id(String _id) { this._id = _id; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getEstilos() { return estilos; }
+    public void setEstilos(String estilos) { this.estilos = estilos; }
+
+    public int getAnioFundacion() { return anioFundacion; }
+    public void setAnioFundacion(int anioFundacion) { this.anioFundacion = anioFundacion; }
+
+    public boolean isEstaActivo() { return estaActivo; }
+    public void setEstaActivo(boolean estaActivo) { this.estaActivo = estaActivo; }
 }
--- discografia/src/main/java/org/iplacex/proyectos/discografia/artistas/ArtistaController.java
+++ discografia/src/main/java/org/iplacex/proyectos/discografia/artistas/ArtistaController.java
@@ -16,10 +16,10 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/api")
 @CrossOrigin
-
 public class ArtistaController {
 
     @Autowired
@@ -30,31 +30,30 @@ public class ArtistaController {
         consumes = MediaType.APPLICATION_JSON_VALUE,
         produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public ResponseEntity<Artista> HandleInsertArtistaRequest(@ResquestBody Artista artista) {
+    public ResponseEntity<Artista> HandleInsertArtistaRequest(@RequestBody Artista artista) {
         Artista temp = artistaRepo.save(artista);
-        return new ResponseEntity<>(temp, null, HttpStatus.CREATED);
+        return new ResponseEntity<>(temp, HttpStatus.CREATED);
     }
 
     @GetMapping(
         value = "/artista/{id}",
         produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public ResponseEntity<Artista> HandleInsertArtistaRequest(@PathVariable("id") String id) {
+    public ResponseEntity<Artista> HandleGetArtistaById(@PathVariable("id") String id) {
         Optional<Artista> temp = artistaRepo.findById(id);
-        if (temp.isPresent()) {
-            return new ResponseEntity<>(null, HttpStatus.OK);
-        } 
-        return null;
+        return temp.map(artista -> new ResponseEntity<>(artista, HttpStatus.OK))
+                   .orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
     }
+
     @GetMapping(
         value = "/artistas",
         produces = MediaType.APPLICATION_JSON_VALUE
     )
     public ResponseEntity<List<Artista>> HandleGetArtistasRequest() {
         List<Artista> artistas = artistaRepo.findAll();
-        return new ResponseEntity<>(artistas, null, HttpStatus.OK);
+        return new ResponseEntity<>(artistas, HttpStatus.OK);
     }
-    
+
     @PutMapping(
         value = "/artista/{id}",
         consumes = MediaType.APPLICATION_JSON_VALUE,
@@ -65,12 +64,12 @@ public class ArtistaController {
         @RequestBody Artista artista
     ) {
         if(!artistaRepo.existsById(id)) {
-            return new ResponseEntity<>(null, null, HttpStatus.NOT_FOUND);
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
         }
 
-        artista._id = id;
+        artista.set_id(id);
         Artista temp = artistaRepo.save(artista);
-        return new ResponseEntity<>(temp, null, HttpStatus.OK);
+        return new ResponseEntity<>(temp, HttpStatus.OK);
     }
 
     @DeleteMapping(
@@ -83,6 +82,6 @@ public class ArtistaController {
         }
 
         artistaRepo.deleteById(id);
-        return new ResponseEntity<>(null, null, HttpStatus.OK);
+        return new ResponseEntity<>(null, HttpStatus.OK);
     }
 }
